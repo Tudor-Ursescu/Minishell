9d36ef5fc732932afe0bc18718a6766162f4e594
Christoph Konnecke <ckonneck@c2r6s2.42wolfsburg.de>
2024-10-08 11:18:07 +0200

    fixed minor leak

--- execution.h
+++ execution.h
@@ -6,13 +6,14 @@
 /*   By: ckonneck <ckonneck@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/16 13:41:34 by ckonneck          #+#    #+#             */
-/*   Updated: 2024/10/07 12:14:29 by ckonneck         ###   ########.fr       */
+/*   Updated: 2024/10/08 11:04:00 by ckonneck         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EXECUTION_H
 # define EXECUTION_H
 # include "libft/libft.h"
+# include "parsing.h"
 # include <dirent.h>
 # include <errno.h>
 # include <fcntl.h>
@@ -24,54 +25,52 @@
 # include <string.h>
 # include <sys/stat.h>
 # include <sys/wait.h>
-# include "parsing.h"
 // extern int g_sig;
 
-typedef void			(*command_func)(char **argv, char **envp);
+typedef void		(*command_func)(char **argv, char **envp);
 typedef struct t_firstcmd
 {
-	char				*name;
-	command_func		func;
-}						t_firstcmd;
+	char			*name;
+	command_func	func;
+}					t_firstcmd;
 
-
-char					*prompt(void);
-void					echo(char **argv, char **envp);
-void					echo2(char **argv, int i);
-int						echo3(char **argv, char **envp);
-void					free_call(char **argv, char *input);
-void					cd_function(char **argv, char **envp);
-void					pwd_function(char **argv, char **envp);
-void					lstatcheck(char **argv);
-t_firstcmd				*init_command_table(void);
-void					env_function(char **argv, char **envp);
-int						handle_env(char *arg, char **envp);
-int						handle_n_flag(char *arg, char **envp);
-void					export_function(char **argv, char **envp);
-void					sort_env(char **env);
-void					exportnullarg(char **envp);
-void					set_env_variable(const char *var, const char *value,
-							char **envp);
-char					**copy_env(char **env);
-char					*find_path(const char *cmd);
-void					free_tokensexec(char **tokens);
-void	execute_path(t_cmd *cmd_list, char **envp);
-int						check_fork(int *pid);
-char					*stitching(char **tokens, const char *cmd);
-void					exit_function(t_firstcmd *command_table, char **argv,
-							char *input);
-void					print_error_toast(void);
-void					input_redirect(char **argv, char **envp);
-void					output_redirect(char **argv, char **envp);
-void					output_append(char **argv, char **envp);
-void					heredoc(char **argv, char **envp);
-void	handle_redirect_or_execute(t_cmd *cmd_list, char **envp);
-int						checkheredoc(char *input, char **argv, int temp_fd);
-void	handle_pipe(t_cmd *cmd_list, int number_of_pipes, char **envp);
-void					execute_absolute(char *path, char **argv, char **envp);
-void					execute_relative(char *path, char **argv, char **envp);
-void					setup_signal_handlers(void);
-void					handle_sigint(int signum);
-void					mini_newline(int signum);
-void	load_ammo(int signum);
+char				*prompt(void);
+void				echo(char **argv, char **envp);
+void				echo2(char **argv, int i);
+int					echo3(char **argv, char **envp);
+void				free_call(char **argv, char *input);
+void				cd_function(char **argv, char **envp);
+void				pwd_function(char **argv, char **envp);
+void				lstatcheck(char **argv);
+t_firstcmd			*init_command_table(void);
+void				env_function(char **argv, char **envp);
+int					handle_env(char *arg, char **envp);
+int					handle_n_flag(char *arg, char **envp);
+void				export_function(char **argv, char **envp);
+void				sort_env(char **env);
+void				exportnullarg(char **envp);
+void				set_env_variable(const char *var, const char *value,
+						char **envp);
+char				**copy_env(char **env);
+char				*find_path(const char *cmd);
+void				free_tokensexec(char **tokens);
+void				execute_path(t_cmd *cmd_list, char **envp);
+int					check_fork(int *pid);
+char				*stitching(char **tokens, const char *cmd);
+void    exit_function(t_firstcmd *command_table, char **argv, char *input);
+void				print_error_toast(void);
+void				input_redirect(char **argv, char **envp);
+void				output_redirect(char **argv, char **envp);
+void				output_append(char **argv, char **envp);
+void				heredoc(char **argv, char **envp);
+void				handle_redirect_or_execute(t_cmd *cmd_list, char **envp);
+int					checkheredoc(char *input, char **argv, int temp_fd);
+void				handle_pipe(t_cmd *cmd_list, int number_of_pipes,
+						char **envp);
+void				execute_absolute(char *path, char **argv, char **envp);
+void				execute_relative(char *path, char **argv, char **envp);
+void				setup_signal_handlers(void);
+void				handle_sigint(int signum);
+void				mini_newline(int signum);
+void				load_ammo(int signum);
 #endif
--- minishell.c
+++ minishell.c
@@ -6,7 +6,7 @@
 /*   By: ckonneck <ckonneck@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/16 14:27:51 by ckonneck          #+#    #+#             */
-/*   Updated: 2024/10/07 15:45:48 by ckonneck         ###   ########.fr       */
+/*   Updated: 2024/10/08 11:17:24 by ckonneck         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@ int	main(int argc, char **argv, char **envp)
 	(void)argv;
 	t_token *token_list;
 	t_cmd	*cmd_list;
+
 	cmd_list = NULL;
 	token_list = NULL;
 	char		*line;
@@ -32,13 +33,14 @@ int	main(int argc, char **argv, char **envp)
 	while (1)
 	{
 		line = prompt();
-
+		
 		if (strcmp(line, "env") == 0)
 		{
 			ft_env(envp);
 			continue;
 		}
 		token_list = tokenize(line);
+		t_token *temp = token_list;
 		if (token_list == NULL)
 		{
 			if (line)
@@ -63,8 +65,6 @@ int	main(int argc, char **argv, char **envp)
 			return (0);
 		}
 		
-		
-		
 		found = 0;
 		int pipenumber = 0;
 		argc = 0;
@@ -84,46 +84,49 @@ int	main(int argc, char **argv, char **envp)
 				}
 				i++;
 			}
-			if (ft_strncmp(cmd_list->args[0], "exit", ft_strlen("exit")) == 0)
+			if (ft_strncmp(line, "exit", ft_strlen("exit")) == 0)
+			{
 				exit_function(command_table, cmd_list->args, line);
-			while (token_list)
+			}
+			while (temp)
 			{
 				if (token_list->type == T_PIPE)
 					pipenumber++;
-				token_list = token_list->next;
+				temp = temp->next;
 			}
+			free(temp);
 			if (pipenumber > 0)
 				handle_pipe(cmd_list, pipenumber, envp);
 			else if (!found)
 				handle_redirect_or_execute(cmd_list, envp);
 		}
-		print_cmd_list(cmd_list);
 		free_all(cmd_list, token_list);
 		free(line);
+		// print_cmd_list(cmd_list);
 		// free_call(argv, line);
 	}
 }
 
-void	free_call(char **argv, char *input)
-{
-	int	i;
 
-	free(input);
-	i = 0;
-	while (argv[i] != NULL)
-	{
-		free(argv[i]);
-		i++;
-	}
-	free(argv);
+
+void    free_call(char **argv, char *input)
+{
+    int i;
+    free(input);
+    i = 0;
+    while (argv[i] != NULL)
+    {
+        free(argv[i]);
+        i++;
+    }
+    free(argv);
 }
-
-void	exit_function(t_firstcmd *command_table, char **argv, char *input)
+void    exit_function(t_firstcmd *command_table, char **argv, char *input)
 {
-	printf("GOODBYE NYA\n");
-	free(command_table);
-	free_call(argv, input);
-	exit(0);
+    printf("GOODBYE NYA\n");
+    free(command_table);
+    free_call(argv, input);
+    exit(0);
 }
 
 void	handle_redirect_or_execute(t_cmd *cmd_list, char **envp)
