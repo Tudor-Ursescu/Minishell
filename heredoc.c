327af0a03ab229769b7ae861e1833d1c074c2002
Christoph Konnecke <ckonneck@c1r15s3.42wolfsburg.de>
2024-10-18 16:31:42 +0200

    refactored heredoc and pipeline rename

---
+++ heredoc.c
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   heredoc.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ckonneck <ckonneck@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/25 09:00:39 by ckonneck          #+#    #+#             */
+/*   Updated: 2024/10/18 14:36:18 by ckonneck         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "execution.h"
+
+//BIG FYI In terms of shell redirection, the first argument after <, >, or >> is strictly 
+//interpreted as a file. If it is not a file, the shell will either fail or behave unexpectedly.
+
+void	heredoc(char *red_args)
+{
+	int temp_fd;
+	int saved_stdin;
+	int saved_stdout;
+
+	temp_fd = 0;
+	saved_stdin = dup(STDIN_FILENO);
+	saved_stdout = dup(STDOUT_FILENO);
+	dup2(temp_fd, 1);
+	temp_fd = open("tempfile.txt", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	if (heredoc_loop(temp_fd, saved_stdin, saved_stdout, red_args) == 1)
+		return ;
+	close(temp_fd);
+}
+
+int heredoc_loop(int temp_fd, int saved_stdin, int saved_stdout, char *red_args)
+{
+	char *input;
+	input = NULL;
+	while (1)
+	{
+		input = readline(">");
+		if (!input)
+		{
+			perror("heredoc error\n");
+			close(temp_fd);
+			restore_fds(saved_stdin, saved_stdout);// experimental, might need to revert if it bugs out
+			return (1);
+		}
+		if (checkheredoc(input, temp_fd, red_args) == 0)
+		{	
+			restore_fds(saved_stdin, saved_stdout);
+			break;
+		}
+		write(temp_fd, input, ft_strlen(input));
+		write(temp_fd, "\n", 1);
+		free(input);
+	}
+	return (0);
+}
+
+int	checkheredoc(char *input, int temp_fd, char *red_args)
+{
+	if (ft_strncmp(input, red_args, strlen(red_args)) == 0
+		&& (input[ft_strlen(red_args)] == '\n' || input[ft_strlen(red_args)] == '\0'))
+	{
+		free(input);
+		close(temp_fd);
+		return(0);
+	}
+	return (1);
+}
---
+++ pipeline.c
@@ -0,0 +1,106 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipeline.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ckonneck <ckonneck@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/24 15:48:50 by ckonneck          #+#    #+#             */
+/*   Updated: 2024/10/18 14:36:57 by ckonneck         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "execution.h"
+#include "parsing.h"
+
+void	handle_pipe(t_cmd *cmd_list, char **envp, t_pipeinfo pipeinfo)
+{
+	int	pipefd[2];
+	int	saved_stdin;
+	int	saved_stdout;
+
+	saved_stdin = dup(STDIN_FILENO);
+	saved_stdout = dup(STDOUT_FILENO);
+	if (pipeinfo.number_of_pipes >= 0)
+		pipe(pipefd);
+	pipeinfo.prev_pid = fork();
+	if (pipeinfo.prev_pid == 0)
+		child_function(pipefd, cmd_list, envp, pipeinfo.prev_fd);
+	else
+		parent_function(pipefd, cmd_list, &pipeinfo.prev_fd);
+	if (pipeinfo.number_of_pipes == 0)
+	{
+		restore_fds(saved_stdin, saved_stdout);
+		return ;
+	}
+	pipeinfo.number_of_pipes--;
+	handle_pipe(cmd_list->next, envp, pipeinfo);
+	waitandsave(pipeinfo.prev_pid);
+}
+
+void	check_next(int pipefd[2], t_cmd *cmd_list)
+{
+	if (cmd_list->next)
+	{
+		if (pipe(pipefd) == -1)
+		{
+			perror("pipe");
+			exit(1);
+		}
+	}
+}
+
+void	child_function(int *pipefd, t_cmd *cmd_list, char **envp, int prev_fd)
+{
+	if (prev_fd != -1)
+	{
+		dup2(prev_fd, STDIN_FILENO);
+		close(prev_fd);
+	}
+	if (cmd_list->next)
+	{
+		close(pipefd[0]);
+		dup2(pipefd[1], STDOUT_FILENO);
+		close(pipefd[1]);
+	}
+	handle_redirect_or_execute(cmd_list, envp);
+	if (find_path(cmd_list->args[0]) != NULL)
+		exit(0);
+	else
+		exit(127);
+}
+
+void	parent_function(int *pipefd, t_cmd *cmd_list, int *prev_fd)
+{
+	if (*prev_fd != -1)
+		close(*prev_fd);
+	if (cmd_list->next)
+	{
+		close(pipefd[1]);
+		*prev_fd = pipefd[0];
+	}
+	else
+	{
+		close(pipefd[0]);
+		close(pipefd[1]);
+	}
+}
+
+t_pipeinfo	initialize_pipeinfo(t_token *token_list)
+{
+	t_pipeinfo	pipeinfo;
+	t_token		*temp;
+
+	temp = token_list;
+	pipeinfo.number_of_pipes = 0;
+	while (temp)
+	{
+		if (temp->type == T_PIPE)
+			pipeinfo.number_of_pipes++;
+		temp = temp->next;
+	}
+	free(temp);
+	pipeinfo.prev_fd = -1;
+	pipeinfo.prev_pid = -1;
+	return (pipeinfo);
+}
